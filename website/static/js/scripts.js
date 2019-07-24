44653c50ddc7c7de4624ff96f67030794f92045f
kiiinhos <viniciuscamargo713@gmail.com>
2019-07-24 16:47:52 -0300

    ideias malucas

---
+++ website/static/js/scripts.js
@@ -0,0 +1,1 @@
+alert('SOU O OBEDE')
---
+++ website/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    args = {
+
+    }
+    return render(request,'index.html',args)
+
+def sobre(request):
+    args = {
+
+    }
+    return render(request,'sobre.html',args)
+     
+    
